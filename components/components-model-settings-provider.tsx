3a344698173b6f2829efe9f07a98d6d524ce8b2e
Rodrigo Meireles de Oliveira <rodrigomeirelesdeoliveira@gmail.com>
2024-09-22 11:01:47 -0300

    Prod ready model settings

--- components/components-model-settings-provider.tsx
+++ components/components-model-settings-provider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { createContext, useState, useContext } from 'react'
-import { ModelSettings } from './ModelSettings'
+import { ModelSettings } from './model-settings'
 
 const ModelSettingsContext = createContext<{
   settings: ModelSettings
--- components/model-settings.tsx
+++ components/model-settings.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Slider } from "@/components/ui/slider"
 
